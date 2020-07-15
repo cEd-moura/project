1734786d21f634283b561ea9a94a8abd585f677f
cEd-moura <cEd-moura@users.noreply.github.com>
2020-07-15 01:48:02 -0300

    adicionado nodemon

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+//Initializg app    
 const app = express();
 
+//Connecting to DB
+mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
 
+//Defining routes
 app.get('/', (req, res) => {
-    res.send('./index.hmtl');
+    res.send('Hello');
 });
-
+//
 app.listen(3001);
