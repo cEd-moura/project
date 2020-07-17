f7bdec32e1b70df884881a6f3e055637fe42e948
cEd-moura <cEd-moura@users.noreply.github.com>
2020-07-17 20:20:53 -0300

    moved server.js to src

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+//Initializg app    
+const app = express();
+
+//Allow server to use json format
+app.use(express.json());
+
+//Liberando CORS
+app.use(cors());
+
+//Connecting to DB
+mongoose.connect('mongodb://localhost:27017/store', { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Routes
+app.use('/', require("./routes"));
+
+
+app.listen(3001);
